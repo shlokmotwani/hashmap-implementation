95e0ecd3cd3f5c5e2ae197fd246b56dbd34fc1e2
Shlok Motwani <shlok.codes@gmail.com>
2024-08-06 12:38:25 +0530

    feat: all tasks implemented except HashSet

--- src/hashmap.js
+++ src/hashmap.js
@@ -1,6 +1,7 @@
 class HashMap {
   constructor() {
-    this.buckets = new Array(16);
+    this.arrayCapacity = 16;
+    this.buckets = new Array(this.arrayCapacity);
     this.loadFactor = 0.75;
   }
 
@@ -17,36 +18,73 @@ class HashMap {
   }
 
   set(key, value) {
+    let newObject = { key: key, value: value, next: null };
     let hashCode = this.hash(key);
-    if (this.buckets[hashCode]) {
-      this.buckets[hashCode] = { key, value };
-      return `(${key}, ${value}) pair overwritten.`;
+    let temp = this.buckets[hashCode];
+
+    if (!temp) {
+      this.buckets[hashCode] = newObject;
+      this.expandIfOverloaded();
+      return `(${key}, ${value}) pair set as HEAD.`;
     }
-    this.buckets[hashCode] = { key, value };
-    return `(${key}, ${value}) pair set.`;
+    let prev;
+    while (temp) {
+      if (temp.key === key) {
+        temp.value = value;
+        return;
+      }
+      prev = temp;
+      temp = temp.next;
+    }
+
+    prev.next = newObject;
+    this.expandIfOverloaded();
+    return `(${key}, ${value}) added to LINKED LIST.`;
   }
 
   get(key) {
     let hashCode = this.hash(key);
-    if (this.buckets[hashCode]) {
-      return this.buckets[hashCode].value;
+    let temp = this.buckets[hashCode]; 
+    if (temp) {
+      return temp.value;
     }
     return null;
   }
 
   has(key) {
     let hashCode = this.hash(key);
-    if (this.buckets[hashCode] && this.buckets[hashCode].key === key) {
-      return true;
+    let temp = this.buckets[hashCode];
+    if (temp) {
+      while (temp) {
+        if (temp.key === key) {
+          return true;
+        }
+        temp = temp.next;
+      }
     }
     return false;
   }
 
   remove(key) {
     let hashCode = this.hash(key);
-    if (this.buckets[hashCode]) {
-      this.buckets[hashCode] = null;
-      return true;
+    let head = this.buckets[hashCode];
+    if (head) {
+      let prev = head;
+      let temp = head.next;
+      if (head.key === key) {
+        this.buckets[hashCode] = head.next;
+        head.next = null;
+        return true;
+      }
+      while (temp) {
+        if (temp.key === key) {
+          prev.next = temp.next;
+          temp.next = null;
+          return true;
+        }
+        temp = temp.next;
+        prev = prev.next;
+      }
     }
     return false;
   }
@@ -54,8 +92,12 @@ class HashMap {
   length() {
     let count = 0;
     for (let i = 0; i < this.buckets.length; i++) {
-      if (this.buckets[i]) {
-        count++;
+      let temp = this.buckets[i];
+      if (temp) {
+        while (temp) {
+          count++;
+          temp = temp.next;
+        }
       }
     }
     return count;
@@ -68,8 +110,12 @@ class HashMap {
   keys() {
     let arr = [];
     for (let i = 0; i < this.buckets.length; i++) {
-      if (this.buckets[i]) {
-        arr.push(this.buckets[i].key);
+      let temp = this.buckets[i];
+      if (temp) {
+        while (temp) {
+          arr.push(temp.key);
+          temp = temp.next;
+        }
       }
     }
     return arr;
@@ -78,8 +124,12 @@ class HashMap {
   values() {
     let arr = [];
     for (let i = 0; i < this.buckets.length; i++) {
-      if (this.buckets[i]) {
-        arr.push(this.buckets[i].value);
+      let temp = this.buckets[i];
+      if (temp) {
+        while (temp) {
+          arr.push(temp.value);
+          temp = temp.next;
+        }
       }
     }
     return arr;
@@ -88,13 +138,63 @@ class HashMap {
   entries() {
     let arr = [];
     for (let i = 0; i < this.buckets.length; i++) {
-      if (this.buckets[i]) {
-        let obj = {};
-        obj[this.buckets[i].key] = this.buckets[i].value;
-        arr.push(obj);
+      let temp = this.buckets[i];
+      if (temp) {
+        while (temp) {
+          arr.push(temp);
+          temp = temp.next;
+        }
       }
     }
     return arr;
+  }
+
+  printMe() {
+    for (let i = 0; i < this.buckets.length; i++) {
+      let temp = this.buckets[i];
+      if (temp) {
+        let hashCode = this.hash(temp.key);
+        let str = `${hashCode} : `;
+        while (temp) {
+          str += `{ ${temp.key} ${temp.value} } -> `;
+          temp = temp.next;
+        }
+        str += "null";
+        console.log(str);
+      }
+    }
+  }
+
+  expandIfOverloaded() {
+    let isOverLoaded = this.length() >= this.loadFactor * this.arrayCapacity;
+    if (!isOverLoaded) {
+      return;
+    }
+    console.log("HashMap OVERLOADED");
+    console.log("HashMap BEFORE OVERLOAD");
+    this.printMe();
+
+    //temporarily push all hashmap objects to an array
+    let tempArray = [];
+    for (let i = 0; i < this.buckets.length; i++) {
+      let temp = this.buckets[i];
+      if (temp) {
+        while (temp) {
+          let objectToPush = { key: temp.key, value: temp.value, next: null };
+          tempArray.push(objectToPush);
+          temp = temp.next;
+        }
+      }
+    }
+
+    this.arrayCapacity *= 2;
+    this.buckets = new Array(this.arrayCapacity);
+    for (let i = 0; i < tempArray.length; i++) {
+      this.set(tempArray[i].key, tempArray[i].value);
+    }
+
+    console.log("HashMap AFTER OVERLOAD");
+    this.printMe();
   }
 }
 
--- src/script.js
+++ src/script.js
@@ -1,7 +1,6 @@
 import { HashMap } from "./hashmap";
 
 let test = new HashMap();
-
 test.set('apple', 'red')
 test.set('banana', 'yellow')
 test.set('carrot', 'orange')
@@ -14,16 +13,21 @@ test.set('ice cream', 'white')
 test.set('jacket', 'blue')
 test.set('kite', 'pink')
 test.set('lion', 'golden')
-test.set('moon', 'silver')
 
-test.set('XXXhat', 'black')
-test.set('XXXice cream', 'white')
-test.set('XXXjacket', 'blue')
-test.set('XXXkite', 'pink')
-test.set('XXXlion', 'golden')
-test.set('XXXmoon', 'silver')
 
-console.log(test.entries());
+console.log("------------------------");
+test.printMe();
+console.log("------------------------");
+console.log("Length: " + test.length());
+console.log("Has apple?: " + test.has("apple"));
+console.log("Has lion?: " + test.has("dog"));
+console.log("Has kitty?: " + test.has("kitty"));
+console.log("Has umbrella?: " + test.has("umbrella"));
 
-test.set('lion', 'white')
+console.log("Lion Removed? : " + test.remove("lion"));
+
+console.log("Keys = " + test.keys());
+console.log("Values = " + test.values());
+
+console.log("------- Entries are as follows -------------")
 console.log(test.entries());
